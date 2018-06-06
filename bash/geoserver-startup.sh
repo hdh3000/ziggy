532ad4c60deb211bdba97fde991ee1c25271bc1b
Henry Hutcheson <henry@fullstory.com>
2018-06-06 16:38:10 -0400

    added geoserver vector tiles plugin

--- bash/geoserver-startup.sh
+++ bash/geoserver-startup.sh
@@ -6,7 +6,7 @@ docker run --name="cloudsql" -d -v /Users/hdh/bin/cloud_sql_proxy.linux.amd64:/c
 -v ~/UNSAFE_PERSONAL/habitat-dev-client.json:/config -p 5432:5432 \
 gcr.io/cloudsql-docker/gce-proxy:1.11 /cloudsql -instances=hdh-habitat-modeling:us-west1:gis=tcp:0.0.0.0:5432 -credential_file=/config
 
-docker run --name "geoserver" -v ~/ziggy-data/geoserver-data:/opt/geoserver/data_dir -p 8080:8080 -d -t kartoza/geoserver
+docker run --name "geoserver" -v ~/ziggy-data/geoserver-data:/opt/geoserver/data_dir -p 8080:8080 -d -t geoserver/test
 
 docker network connect geo-net cloudsql
 docker network connect geo-net geoserver
